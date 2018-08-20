5aa6bc09dadbdcf96a55940c61cbc2e95bcefc56
Mike Makowski <m.makowski@adtype.com>
2018-08-20 17:41:16 +0200

    Some fixes

--- lib/Braintree.ts
+++ lib/Braintree.ts
@@ -9,57 +9,62 @@ import IConfig from './interfaces/IConfig';
 
 export default class Braintree {
 
-  private config: IConfig;
-  private gateway: any;
+    private config: IConfig;
+    private gateway: any;
 
-  /**
-   * Braintree Module constructor
-   * @param config Config for the braintree
-   */
-  constructor(config: IConfig) {
-    this.config = config;
-    return this;
-  }
-
-  public getConfig(key: string | null = null) {
-    switch ((key) ? key : null) {
-        case 'environment':
-            return this.config.environment;
-        case 'merchantId':
-            return this.config.merchantId;
-        case 'publicKey':
-            return this.config.publicKey;
-        case 'privateKey':
-            return this.config.privateKey;
-        default:
-            return this.config as IConfig;
+    /**
+     * Braintree Module constructor
+     * @param config Config for the braintree
+     */
+    constructor(config: IConfig) {
+        this.config = config;
+        return this;
     }
-  }
 
-  public connect() {
-    if (!this.config) { return false; }
-    this.gateway = braintree.connect(this.config);
-    if (!this.gateway) { return false; }
-    return true;
-  }
+    public getConfig(key: string | null = null) {
+        switch ((key) ? key : null) {
+            case 'environment':
+                return this.config.environment;
+            case 'merchantId':
+                return this.config.merchantId;
+            case 'publicKey':
+                return this.config.publicKey;
+            case 'privateKey':
+                return this.config.privateKey;
+            default:
+                return this.config as IConfig;
+        }
+    }
 
-  public getGateway() {
-    return this.gateway;
-  }
+    public connect() {
+        if (!this.config) { return false; }
+        this.gateway = braintree.connect(this.config);
+        if (!this.gateway) { return false; }
+        return true;
+    }
 
-  public getModule(moduleName: string) {
-      moduleName = moduleName.toLowerCase();
-      switch (moduleName) {
-        case 'customer':
-            return new Customer(this.getGateway().customer);
-        case 'subscription':
-            return new Subscription(this.getGateway().subscription);
-        case 'plan':
-            return new Plan(this.getGateway().plan);
-        case 'transaction':
-            return new Transaction(this.getGateway().transaction);
-        default:
-            throw new Error('There is no module with that name!');
-      }
-  }
+    public getGateway() {
+        return this.gateway;
+    }
+
+    public getModule(moduleName: string) {
+        moduleName = moduleName.toLowerCase();
+
+        if (this.getGateway().hasOwnProperty(moduleName)) {
+            const gateway = this.getGateway()[moduleName];
+
+            switch (moduleName) {
+                case 'customer':
+                    return new Customer(gateway);
+                case 'subscription':
+                    return new Subscription(gateway);
+                case 'plan':
+                    return new Plan(gateway);
+                case 'transaction':
+                    return new Transaction(gateway);
+            }
+        }
+
+        return undefined;
+    }
 }
