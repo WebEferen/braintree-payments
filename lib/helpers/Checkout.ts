7c214fcda492611a1153c9cf193a27993a3d0b69
Mike Makowski <m.makowski@adtype.com>
2018-08-24 11:23:54 +0200

    Added some documentation into checkout helper

--- lib/helpers/Checkout.ts
+++ lib/helpers/Checkout.ts
@@ -8,22 +8,41 @@ export default class Checkout {
 
   private braintree: any;
 
+  /**
+   * Constructor
+   * @param config Braintree config
+   */
   constructor(config: IConfig) {
     this.braintree = new Braintree(config);
   }
 
-  public async generateToken() {
+  /**
+   * Generates client token for the drop-in authorization
+   */
+  public async generateClientToken() {
     const clientTokenModule = this.braintree.getModule('clientToken');
     const token = await clientTokenModule.generate();
     return token;
   }
 
+  /**
+   * Subscribes specific user to the specific plan with addons
+   * @param paymentMethodNonce Nonce from the drop-in UI
+   * @param planId Specific unique plan index
+   * @param addOns Addons array
+   */
   public async subscribe(paymentMethodNonce: string, planId: string, addOns: IAddOn[]) {
     const subscriptionModule = this.braintree.getModule('subscription');
     const subscribe = await subscriptionModule.create({paymentMethodNonce, planId, addOns});
     return subscribe;
   }
 
+  /**
+   * Creates specific transaction base on the nonce provided
+   * @param paymentMethodNonce Nonce from the drop-in UI
+   * @param amount Specific amount
+   * @param customer Customer to create
+   */
   public async sale(paymentMethodNonce: string, amount: number, customer: ICustomer) {
     const transactionModule = this.braintree.getModule('transaction');
     const transaction = await transactionModule.sale({paymentMethodNonce, amount, customer});
